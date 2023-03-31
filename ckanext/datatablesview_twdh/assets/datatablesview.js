adbdcac6c2b18423b5cc51c808744d7dda6959e5
Ben Bright <ben.bright@twdb.texas.gov>
2023-03-31 19:56:16 +0000

    Cleanup; Paging; Table height; Summary Statistics WIP

--- ckanext/datatablesview_twdh/assets/datatablesview.js
+++ ckanext/datatablesview_twdh/assets/datatablesview.js
@@ -1,4 +1,5 @@
 var table_rows_per_page = 100;
+var resizeTO;
 
 var run_query = function(params, format) {
 
@@ -15,7 +16,6 @@ var run_query = function(params, format) {
   var f = $('<input name="format" type="hidden"/>');
   f.attr("value", format);
   form.append(f);
-
   form.submit();
 }
 
@@ -27,37 +27,20 @@ this.ckan.module('datatablesview_twdh', function (jQuery) {
       // tags in templates/datatables/datatables_view.html
       var datatable = jQuery('#dtprv').DataTable({
 
-        // turn on desired visible dom components
-        // dom: 'BQlfrtip',
-        // dom: 'QBlfrtip',
-        // dom: 'QBipfrt',
-
-        // turn on search highlighting
+        // Setup search highlighting
         mark: true,
 
-        // turn on row selection
+        // Setup row selection
         select: true,
 
-
-        columnDefs: [ {
-          orderable: true,
-          className: 'select-checkbox',
-          targets:   0
-        } ],
-        select: {
-          style:    'os',
-          selector: 'td:first-child'
-        },
         order: [[ 1, 'asc' ]],
-/*
-*/
 
-        // turn on row searchBuilder
+        // Setup searchBuilder
         searchBuilder: {
           depthLimit: 2
         },
 
-        // turn on column reordering
+        // Set column reordering
         colReorder: false,
 
         // Set search placeholder text
@@ -71,20 +54,9 @@ this.ckan.module('datatablesview_twdh', function (jQuery) {
 
 
         lengthMenu: [
-          [ 10, 25, 50, 100, 1000 ],
-          [ '10', '25', '50', '100', '1,000' ]
+          [ 10, 100, 1000 ],
+          [ '10', '100', '1,000' ]
         ],
-
-        // ajax: "/datatables/ajax/c0c41b19-8373-4bef-8482-d1d1cd214514",
-
-        /*
-
-        "ajax": {
-          "url": "/datatables/ajax/c0c41b19-8373-4bef-8482-d1d1cd214514",
-          "type": "POST",
-          "data": ""
-        },
-        */
 
         // turn on scroller
         scrollY:        "60vh",
@@ -92,7 +64,6 @@ this.ckan.module('datatablesview_twdh', function (jQuery) {
         scrollCollapse: true,
         scroller:       true,
 
-        // buttons:['copy','colvis','print','createState', 'savedStates'],
         buttons:[
           'copy',
           'csvHtml5',
@@ -106,28 +77,16 @@ this.ckan.module('datatablesview_twdh', function (jQuery) {
           'excelHtml5',
           'colvis',
           'print',
-          /*
-          {
-            extend: 'searchBuilder',
-            config: {
-                depthLimit: 2
-            }
-          }
-          */
 
         ],
-        // buttons:['colvis','excel','print','searchBuilder'],
-        // buttons:['colvis','print','createState', 'savedStates'],
-        // buttons:['colvis','print'],
 
-        // "sPaginationType": "extStyle",
-        paging: false,
         pagingType: 'full_numbers',
         "pageLength": table_rows_per_page,
 
 
         "initComplete": function(settings, json) {
           console.log( 'DataTables has finished its initialisation.' );
+          fit_datatable_to_window();
         },
 
         search: {
@@ -139,10 +98,38 @@ this.ckan.module('datatablesview_twdh', function (jQuery) {
 
         // turn on table metadata display
         infoCallback: function( settings, start, end, max, total, pre ) {
+
+          var api = this.api();
+ 
+          // Output the data for the visible rows to the browser's console
+          console.log( api.rows( {page:'current'} ).data() );
+          console.log( api.rows( {page:'current'} ).data().length );
+          console.log( table_rows_per_page );
+
+          console.log( $('select[name="dtprv_length"]').val() );
+
+          var rows_per_page = $('select[name="dtprv_length"]').val();
+
+          if( total <= rows_per_page ) {
+
+            console.log( 'hide pagination' );
+            $( '.dataTables_paginate' ).hide();
+
+          } else {
+
+            console.log( 'show pagination' );
+            $( '.dataTables_paginate' ).show();
+
+          }
+
+
+
           return total + " record" + ( total != 1 ? 's' : '' );
         },
 
         headerCallback: function( thead, data, start, end, display ) {
+
+
 
           $(thead).find('th').eq(0).html( '' );
 
@@ -179,29 +166,7 @@ this.ckan.module('datatablesview_twdh', function (jQuery) {
             });
           }
 
-
-
-
         },
-
-        // "bAutoWidth": false,
-
-        /*
-        // truncate cell text to 50 characters and add tooltip for remainder
-        columnDefs: [ {
-          targets: '_all',
-          render: $.fn.dataTable.render.ellipsis( 50, true, true )
-        } ],
-        */
-
-        /*
-        columnDefs: [
-          {
-              targets: 1,
-              render: DataTable.render.datetime('MMM Do YYYY'),
-          },
-        ],
-        */
 
         /* 
           stateSaveCallback and stateLoadCallback are configured here in order to allow us to have a 'share' link for table state
@@ -323,24 +288,45 @@ this.ckan.module('datatablesview_twdh', function (jQuery) {
     } );
 
 
+    function fit_datatable_to_window() {
+
+      clearTimeout( resizeTO );
+      resizeTO = setTimeout(function() { 
+
+
+        console.log( 'DT resize' );
+
+        var tools_height = 
+          $( '#dtprv_wrapper .dtsb-searchBuilder' ).height()
+          + $( '#dtprv_wrapper .dt-buttons' ).height()
+          + $( '#dtprv_wrapper #dtprv_status' ).height()
+          + $( '#dtprv_wrapper .dataTables_paginate' ).height()
+          + $( '#dtprv_wrapper .dataTables_scrollHead' ).height()
+        ;
+  
+        var iframe_height = 
+          $( window ).height();
+  
+        var table_height = iframe_height - tools_height - 18;
+
+        /*
+        console.log( tools_height );
+        console.log( iframe_height );
+        console.log( table_height );
+        */
+       
+        $( '.dataTables_scrollBody' ).css( 'max-height', '100vh' );
+        $( '.dataTables_scrollBody' ).height( table_height );
+  
+      }, 1000 );
+
+    }
 
       /* set event listeners */
 
-      var resizeTO;
       $( window ).on( 'resize' , function () {
 
-
-        // send message to parent window to set frame height
-        clearTimeout( resizeTO );
-        resizeTO = setTimeout(function() { 
-          // console.log( 'DataTable window resize postMessage occurred at: '+new Date().getTime() );
-          // console.log( $( '#dtprv_wrapper' ).height() );
-          window.parent.postMessage({ frameHeight: $( '#dtprv_wrapper' ).height() }, '*'); 
-          // window.parent.postMessage({ frameHeight: 1200 }, '*'); 
-        }, 1000 );
-
-        // send another message 1 second later to clean up because sometimes the first height is a miscalculation
-        // setTimeout(function() { window.parent.postMessage({ frameHeight: $( '#dtprv_wrapper' ).height() }, '*'); }, 1000 );
+        fit_datatable_to_window()
         
       });
 
@@ -416,303 +402,3 @@ this.ckan.module('datatablesview_twdh', function (jQuery) {
 
   }
 });
-
-/**
-* This pagination plug-in provides pagination controls for DataTables which
-* match the style and interaction of the ExtJS library's grid component.
-*
-*  @name ExtJS style
-*  @summary Pagination in the styling of ExtJS
-*  @author [Zach Curtis](http://zachariahtimothy.wordpress.com/)
-*
-*  @example
-*    $(document).ready(function() {
-*        $('#example').dataTable( {
-*            "sPaginationType": "extStyle"
-*        } );
-*    } );
-*/
-
-$.fn.dataTableExt.oApi.fnExtStylePagingInfo = function ( oSettings )
-{
-  return {
-    "iStart":         oSettings._iDisplayStart,
-    "iEnd":           oSettings.fnDisplayEnd(),
-    "iLength":        oSettings._iDisplayLength,
-    "iTotal":         oSettings.fnRecordsTotal(),
-    "iFilteredTotal": oSettings.fnRecordsDisplay(),
-    "iPage":          oSettings._iDisplayLength === -1 ?
-      0 : Math.ceil( oSettings._iDisplayStart / oSettings._iDisplayLength ),
-    "iTotalPages":    oSettings._iDisplayLength === -1 ?
-      0 : Math.ceil( oSettings.fnRecordsDisplay() / oSettings._iDisplayLength )
-  };
-};
-
-$.fn.dataTableExt.oPagination.extStyle = {
-
-  "fnInit": function (oSettings, nPaging, fnCallbackDraw) {
-
-    var oPaging = oSettings.oInstance.fnExtStylePagingInfo();
-
-    var nFirst = $('<span/>', { 'class': 'paginate_button first' , text : "" }).append('<i class="fa fa-angle-double-left" aria-hidden="true"></i>');
-    var nPrevious = $('<span/>', { 'class': 'paginate_button previous' , text : "" }).append('<i class="fa fa-angle-left" aria-hidden="true"></i>');
-    var nNext = $('<span/>', { 'class': 'paginate_button next' , text : "" }).append('<i class="fa fa-angle-right" aria-hidden="true"></i>');
-    var nLast = $('<span/>', { 'class': 'paginate_button last' , text : "" }).append('<i class="fa fa-angle-double-right" aria-hidden="true"></i>');
-    // nPageTxt = $("<span />", { text: '' });
-
-    // nPageNumBox = $('<input />', { type: 'text', val: 1, 'size': 4, 'class': 'paginate_input_box' });
-
-    
-    var nStartRowBox = $('<span />', { type: 'text', text: 1, 'size': 4, 'class': 'start_row_input_box' });
-    var nRowHyphen = $('<span />', { text: '-', 'class': 'range_separator' });
-    var nEndRowBox = $('<span />', { type: 'text', text: oPaging.iLength, 'size': 4, 'class': 'end_row_input_box' });
-
-
-    // console.log( oPaging );
-    // nPageOf = $('<span />', { text: '/' });
-    // nTotalPages = $('<span />', { class :  "paginate_total" , text : oPaging.iTotalPages });
-
-    // check to make sure the pager is necessary
-    // if( oPaging.iTotal > oPaging.iLength ) {
-      $(nPaging)
-        .append(nFirst)
-        .append(nPrevious)
-        // .append(nPageTxt)
-        // .append(nPageNumBox)
-        .append(nStartRowBox)
-        .append(nRowHyphen)
-        .append(nEndRowBox)
-        // .append(nPageOf)
-        // .append(nTotalPages)
-        .append(nNext)
-        .append(nLast)
-        ;
-    /*
-    } else {
-      // if not hide the container
-      $( '#dtprv_paginate' ).css( 'display', 'none' );
-
-    }
-    */
-
-    nFirst.click(function () {
-      if( $(this).hasClass("disabled") )
-        return;
-      oSettings.oApi._fnPageChange(oSettings, "first");
-      fnCallbackDraw(oSettings);
-    }).bind('selectstart', function () { return false; });
-
-    nPrevious.click(function () {
-      if( $(this).hasClass("disabled") )
-        return;
-      oSettings.oApi._fnPageChange(oSettings, "previous");
-      fnCallbackDraw(oSettings);
-    }).bind('selectstart', function () { return false; });
-
-    nNext.click(function () {
-      if( $(this).hasClass("disabled") )
-        return;
-      oSettings.oApi._fnPageChange(oSettings, "next");
-      fnCallbackDraw(oSettings);
-    }).bind('selectstart', function () { return false; });
-
-    nLast.click(function () {
-      if( $(this).hasClass("disabled") )
-        return;
-      oSettings.oApi._fnPageChange(oSettings, "last");
-      fnCallbackDraw(oSettings);
-    }).bind('selectstart', function () { return false; });
-
-    /*
-    nPageNumBox.change(function () {
-      var pageValue = parseInt($(this).val(), 10) - 1 ; // -1 because pages are 0 indexed, but the UI is 1
-      var oPaging = oSettings.oInstance.fnPagingInfo();
-
-      if(pageValue === NaN || pageValue<0 ){
-        pageValue = 0;
-      }else if(pageValue >= oPaging.iTotalPages ){
-        pageValue = oPaging.iTotalPages -1;
-      }
-      oSettings.oApi._fnPageChange(oSettings, pageValue);
-      fnCallbackDraw(oSettings);
-    });
-    */
-
-  },
-
-
-  "fnUpdate": function (oSettings, fnCallbackDraw) {
-    if (!oSettings.aanFeatures.p) {
-      return;
-    }
-
-    var oPaging = oSettings.oInstance.fnExtStylePagingInfo();
-
-    /* Loop over each instance of the pager */
-    var an = oSettings.aanFeatures.p;
-
-    $(an).find('span.paginate_total').html(oPaging.iTotalPages);
-    $(an).find('.paginate_input_box').val(oPaging.iPage+1);
-
-    $(an).find('.start_row_input_box').html( ( oPaging.iPage * oPaging.iLength ) +1);
-
-    $(an).find('.end_row_input_box').html( 
-      ( oPaging.iPage * oPaging.iLength ) + oPaging.iLength > oPaging.iFilteredTotal ?
-        oPaging.iFilteredTotal : ( oPaging.iPage * oPaging.iLength ) + oPaging.iLength
-    
-    );
-
-    $(an).each(function(index,item) {
-
-      var $item = $(item);
-
-      if (oPaging.iPage == 0) {
-        var prev = $item.find('span.paginate_button.first').add($item.find('span.paginate_button.previous'));
-        prev.addClass("disabled");
-      } else {
-        var prev = $item.find('span.paginate_button.first').add($item.find('span.paginate_button.previous'));
-        prev.removeClass("disabled");
-      }
-
-      if (oPaging.iPage+1 == oPaging.iTotalPages) {
-        var next = $item.find('span.paginate_button.last').add($item.find('span.paginate_button.next'));
-        next.addClass("disabled");
-      } else {
-        var next = $item.find('span.paginate_button.last').add($item.find('span.paginate_button.next'));
-        next.removeClass("disabled");
-      }
-    });
-  }
-};
-
-jQuery.fn.dataTableExt.oApi.fnPagingInfo = function ( oSettings )
-{
-  return {
-    "iStart":         oSettings._iDisplayStart,
-    "iEnd":           oSettings.fnDisplayEnd(),
-    "iLength":        oSettings._iDisplayLength,
-    "iTotal":         oSettings.fnRecordsTotal(),
-    "iFilteredTotal": oSettings.fnRecordsDisplay(),
-    "iPage":          oSettings._iDisplayLength === -1 ?
-      0 : Math.ceil( oSettings._iDisplayStart / oSettings._iDisplayLength ),
-    "iTotalPages":    oSettings._iDisplayLength === -1 ?
-      0 : Math.ceil( oSettings.fnRecordsDisplay() / oSettings._iDisplayLength )
-  };
-};
-
-
-
-/**
- * This data rendering helper method can be useful for cases where you have
- * potentially large data strings to be shown in a column that is restricted by
- * width. The data for the column is still fully searchable and sortable, but if
- * it is longer than a give number of characters, it will be truncated and
- * shown with ellipsis. A browser provided tooltip will show the full string
- * to the end user on mouse hover of the cell.
- *
- * This function should be used with the `dt-init columns.render` configuration
- * option of DataTables.
- *
- * It accepts three parameters:
- *
- * 1. `-type integer` - The number of characters to restrict the displayed data
- *    to.
- * 2. `-type boolean` (optional - default `false`) - Indicate if the truncation
- *    of the string should not occur in the middle of a word (`true`) or if it
- *    can (`false`). This can allow the display of strings to look nicer, at the
- *    expense of showing less characters.
- * 2. `-type boolean` (optional - default `false`) - Escape HTML entities
- *    (`true`) or not (`false` - default).
- *
- *  @name ellipsis
- *  @summary Restrict output data to a particular length, showing anything
- *      longer with ellipsis and a browser provided tooltip on hover.
- *  @author [Allan Jardine](http://datatables.net)
- *  @requires DataTables 1.10+
- *
- * @returns {Number} Calculated average
- *
- *  @example
- *    // Restrict a column to 17 characters, don't split words
- *    $('#example').DataTable( {
- *      columnDefs: [ {
- *        targets: 1,
- *        render: $.fn.dataTable.render.ellipsis( 17, true )
- *      } ]
- *    } );
- *
- *  @example
- *    // Restrict a column to 10 characters, do split words
- *    $('#example').DataTable( {
- *      columnDefs: [ {
- *        targets: 2,
- *        render: $.fn.dataTable.render.ellipsis( 10 )
- *      } ]
- *    } );
- */
-
- jQuery.fn.dataTable.render.ellipsis = function ( cutoff, wordbreak, escapeHtml ) {
-	var esc = function ( t ) {
-		return t
-			.replace( /&/g, '&amp;' )
-			.replace( /</g, '&lt;' )
-			.replace( />/g, '&gt;' )
-			.replace( /"/g, '&quot;' );
-	};
-
-	return function ( d, type, row ) {
-		// Order, search and type get the original data
-		if ( type !== 'display' ) {
-			return d;
-		}
-
-		if ( typeof d !== 'number' && typeof d !== 'string' ) {
-			return d;
-		}
-
-		d = d.toString(); // cast numbers
-
-    // d = d.replace("\n", "<br/>"); // HTML encode newline characters
-
-		if ( d.length <= cutoff ) {
-			return d;
-		}
-
-		var shortened = d.substr(0, cutoff-1);
-
-		// Find the last white space character in the string
-		if ( wordbreak ) {
-			shortened = shortened.replace(/\s([^\s]*)$/, '');
-		}
-
-		// Protect against uncontrolled HTML input
-		if ( escapeHtml ) {
-			shortened = esc( shortened );
-		}
-
-    /* 
-    Set up tooltips so that the first 50% of rows in a page have the tooltipon the bottom and the second 50% of rows have the tooltip on the top.
-    This is necessary because otherwise there are cases where a tooltip falls outside the iframe and is invisible in either the first or last row.
-    This could be set up so that only the first (or last) row changes tooltip direction, but it 'feels' better on the page to have it switch locations at the mid-point
-    */
-
-    var tooltip_location = '';
-    var tooltip_class = '';
-
-    if ( ( row[0] % table_rows_per_page ) / table_rows_per_page > 0.5 | ( row[0] % table_rows_per_page ) == 0  ) {
-
-      tooltip_location = 'top';
-      tooltip_class = 'tooltip-top';
-
-    } else {
-
-      tooltip_location = 'bottom';
-      tooltip_class = 'tooltip-bottom';
-
-    }
-    
-    return '<div class="ellipsis ' + tooltip_class + '" data-toggle="tooltip" data-placement="' + tooltip_location + '" title="'+esc(d)+'">'+shortened+'&#8230;</div>';
-
-	};
-
-};
